72fb1891f37c4307ac9db6b5eea8dc4ccc112b82
Valekta <karuri@web.de>
2017-09-28 00:46:04 +0200

    Add method for person profile

--- dbPraktikum/src/main/java/db/util/PersonRelatedImpl.java
+++ dbPraktikum/src/main/java/db/util/PersonRelatedImpl.java
@@ -1,9 +1,40 @@
 package db.util;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import db.model.Person;
+
 public class PersonRelatedImpl implements PersonRelatedAPI {
 
 	public void getProfile(Long id) {
-		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Query q = session.createQuery("select p from Person p where p.pid = :id");
+			q.setParameter("id", id);
+			Person person = (Person) q.getSingleResult();
+			System.out.println("getProfil");
+			System.out.print("Name: " + person.getFirstName() + " " + person.getLastName() + "\n" 
+					+ "Birthday: " + person.getBirthday() + "\n" + "Gender: " + person.getGender() + "\n" 
+					+ "Location: " + person.getLocatedIn().getName() + ", " + person.getLocatedIn().getCountry().getName() + " ("
+					+ person.getLocatedIn().getCountry().getContinent().getName() + ")\n" +
+					"Languages: ");
+			for(String language : person.getLanguages()) {
+		    	System.out.print(language + "|");
+		    }
+			System.out.println("");
+			System.out.print("Email: ");
+			for(String email : person.getEmails()) {
+		    	System.out.print(email + "|");
+		    }
+			System.out.println("");
+			System.out.println("Join: " + person.getCreationDate().getTime());
+		} catch(Exception e) {
+				System.out.println(e);
+		} finally {
+			System.out.print("\n");
+			session.close();
+		}
 	}
 
 	public void getCommonInterestsOfMyFriends(Long id) {
